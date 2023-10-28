293f6a0fff147e3375d922faf8c40f7f605d3e50
ValeriiKoniushenko <Valerii.Koniushenko@gmail.com>
2023-10-28 21:14:16 +0300

    Init

---
+++ app/src/main/java/com/example/vakon/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.vakon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
