6f27a9dfdc3f61713fe443b9e3bd49b764dec28e
ValeriiKoniushenko <Valerii.Koniushenko@gmail.com>
2023-10-29 10:50:45 +0200

    Added mini-features. Also, added an ability to share this app

--- app/src/main/java/com/example/vakon/MainActivity.java
+++ app/src/main/java/com/example/vakon/MainActivity.java
@@ -7,14 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle actionBarDrawerToggle;
@@ -47,31 +51,74 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        Menu menu = navigationView.getMenu();
+        boolean isLogged = false;
+        if (isLogged)
+        {
+            menu.findItem(R.id.item_profile_login).setVisible(false);
+            menu.findItem(R.id.item_profile_signup).setVisible(false);
+        }
+        else
+        {
+            menu.findItem(R.id.item_profile_logout).setVisible(false);
+        }
+
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                if (id == R.id.item_home)
-                {
-                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT);
-                }
+        navigationView.setNavigationItemSelectedListener(this);
 
-                return false;
-            }
-        });
+        navigationView.setCheckedItem(R.id.item_home);
+    }
+
+    public boolean closeNavigationBar()
+    {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START))
-        {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else
+        if (!closeNavigationBar())
         {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        final int id = item.getItemId();
+        if (id == R.id.item_home)
+        {
+            closeNavigationBar();
+            return true;
+        }
+        else if (id == R.id.main_menu_item_misc_share)
+        {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_TEXT, "You can spy for this cool project. Just download an app: https://github.com/ValeriiKoniushenko/VaKon");
+            intent.setType("text/plain");
+            startActivity(Intent.createChooser(intent, "Share"));
+
+            closeNavigationBar();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Rect viewRect = new Rect();
+        navigationView.getGlobalVisibleRect(viewRect);
+
+        if (!viewRect.contains((int)ev.getRawX(), (int)ev.getRawY()))
+        {
+            closeNavigationBar();
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
